2bf6319cb7d2cc48f44e4b0797a78510bd1549bd
CLJiaHao <clarence.lee@murata.com>
2023-10-30 15:22:54 +0800

    Clean code to consolidate functions to use case

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import {
-  API,
   initialArray,
   initialData,
   initialFocus,
@@ -9,11 +8,10 @@ import {
   initialState,
 } from "./ini";
 
+import saveData from "./utils/saveData";
 import { AppContext } from "./contexts/context";
 import { InfoBar, ImageHolder, Gallery } from "./containers";
-import saveData from "./utils/saveData";
-import dataProcess from "./utils/dataProcess";
-import loadImage from "./utils/loadImage";
+
 import changeColour from "./utils/changeColour";
 
 function App() {
@@ -44,70 +42,6 @@ function App() {
   const save = () => {
     if (!state.error && data.lot_no && Object.values(array).length > 0)
       saveData(array, data, info);
-  };
-
-  const errorHandling = async (res) => {
-    if (res.status === "404") {
-      setState({
-        error: true,
-        image: {
-          src: "Error/Error_Lot_Num.png",
-          alt: "Error_Lot_Num.png",
-        },
-      });
-      return false;
-    } else if (res.status === "400") {
-      setState({
-        error: true,
-        image: { src: "Error/Error.png", alt: "Error.png" },
-      });
-      return false;
-    } else {
-      const json = await res.json();
-      return json;
-    }
-  };
-
-  const process = async (e) => {
-    e.preventDefault();
-    initialize();
-
-    if (data.lot_no === "" || data.lot_no === null || state.error) {
-      data.lot_no = prompt("Please Scan or Input Lot Number.");
-    }
-    const file = e.target.files[0];
-    if (file) {
-      setState({
-        ...state,
-        image: { src: "Error/Loading.gif", alt: "Loading.gif" },
-      });
-      const res = await loadImage(file, data);
-      if (res) {
-        const json = await errorHandling(res);
-        if (json) {
-          const [array_dict, data_dict, focus_dict, info_dict] = dataProcess(
-            json,
-            array,
-            data,
-            focus,
-            info
-          );
-          setArray(array_dict);
-          setData(data_dict);
-          setFocus(focus_dict);
-          setInfo(info_dict);
-          setState({
-            error: false,
-            image: {
-              src: `${API}${data_dict.directory.split("backend")[1]}/Original/${
-                file.name
-              }`,
-              alt: file.name,
-            },
-          });
-        }
-      }
-    }
   };
 
   const highlight = (zone, key) => {
@@ -142,7 +76,7 @@ function App() {
     >
       <div className="App">
         <main className="App-main">
-          <InfoBar process={process} save={save} />
+          <InfoBar initialize={initialize} save={save} />
           <ImageHolder highlight={highlight} />
         </main>
         <aside className="App-side">
--- frontend/src/containers/InfoBar/InfoBar.js
+++ frontend/src/containers/InfoBar/InfoBar.js
@@ -1,13 +1,93 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../contexts/context";
 import "./InfoBar.css";
+import { API } from "../../ini";
+
+import dataProcess from "../../utils/dataProcess";
+import loadImage from "../../utils/loadImage";
 
 import Info from "./components/Info";
 import Input from "./components/Input";
 import Button from "./components/Button";
 
-const InfoBar = ({ process, save }) => {
-  const { data, info } = useContext(AppContext);
+const InfoBar = ({ initialize, save }) => {
+  const {
+    array,
+    data,
+    focus,
+    info,
+    state,
+    setArray,
+    setData,
+    setFocus,
+    setInfo,
+    setState,
+  } = useContext(AppContext);
+
+  const errorHandling = async (res) => {
+    if (res.status === "404") {
+      setState({
+        error: true,
+        image: {
+          src: "Error/Error_Lot_Num.png",
+          alt: "Error_Lot_Num.png",
+        },
+      });
+      return false;
+    } else if (res.status === "400") {
+      setState({
+        error: true,
+        image: { src: "Error/Error.png", alt: "Error.png" },
+      });
+      return false;
+    } else {
+      const json = await res.json();
+      return json;
+    }
+  };
+
+  const process = async (e) => {
+    e.preventDefault();
+    initialize();
+
+    if (data.lot_no === "" || data.lot_no === null || state.error) {
+      data.lot_no = prompt("Please Scan or Input Lot Number.");
+    }
+    const file = e.target.files[0];
+    if (file) {
+      setState({
+        ...state,
+        image: { src: "Error/Loading.gif", alt: "Loading.gif" },
+      });
+      const res = await loadImage(file, data);
+      if (res) {
+        const json = await errorHandling(res);
+        if (json) {
+          const [array_dict, data_dict, focus_dict, info_dict] = dataProcess(
+            json,
+            array,
+            data,
+            focus,
+            info
+          );
+          setArray(array_dict);
+          setData(data_dict);
+          setFocus(focus_dict);
+          setInfo(info_dict);
+          setState({
+            error: false,
+            image: {
+              src: `${API}${data_dict.directory.split("backend")[1]}/Original/${
+                file.name
+              }`,
+              alt: file.name,
+            },
+          });
+        }
+      }
+    }
+  };
+
   return (
     <div className="infobar">
       <Info data={data} info={info} />
